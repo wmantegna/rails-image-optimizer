ef8afb77c7dfa84b764fa9f08085c21db399b7bd
Jason Fleetwood-Boldt <jason@datatravels.com>
2017-02-20 11:36:10 -0500

    amazon setup

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,15 @@ Bundler.require(*Rails.groups)
 
 module ImageOptimWithPaperclipOnHerokuExampleAppRails41
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    
+    s3 = YAML.load_file( "#{Rails.root}/config/amazon_s3.yml")[Rails.env]
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    if !Rails.env.development?      
+      config.paperclip_defaults = {
+        storage: :s3,
+        s3_credentials: s3,
+        s3_protocol: "https",
+      }
+    end
   end
 end
