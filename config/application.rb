2c7e9dd724f0a26faec6a259daddcc9ac5e5e3f9
Jason Fleetwood-Boldt <jason@datatravels.com>
2017-02-20 12:18:43 -0500

    reconfigures S3

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,17 @@ Bundler.require(*Rails.groups)
 module ImageOptimWithPaperclipOnHerokuExampleAppRails41
   class Application < Rails::Application
     
-    s3 = YAML.load_file( "#{Rails.root}/config/amazon_s3.yml")[Rails.env]
+    s3 = {"bucket"=>ENV['AWS_BUCKET'], 
+      "access_key_id"=>ENV['AWS_ACCESS_KEY_ID'], 
+      "secret_access_key"=>ENV['AWS_SECRET_ACCESS_KEY']}
 
-    if !Rails.env.development?      
+    # if !Rails.env.development?      
+      byebug
       config.paperclip_defaults = {
         storage: :s3,
         s3_credentials: s3,
         s3_protocol: "https",
       }
-    end
+    # end
   end
 end
